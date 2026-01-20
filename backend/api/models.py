55cb8a2a51807c010ab07c35b03e3f22018f7bfb
bravera <braverachacha@gmail.com>
2026-01-20 21:22:53 +0000

    hashed verify and reset tokens in user model

--- backend/api/models.py
+++ backend/api/models.py
@@ -17,8 +17,26 @@ class User(db.Model):
     reset_token_expiry = db.Column(db.DateTime, nullable=True)
     reset_token_used = db.Column(db.Boolean, default=False)
     
+    # PASSWORD HASHING
     def set_password(self, password):
         self.password = generate_password_hash(password)
         
     def check_password(self, password):
         return check_password_hash(self.password, password)
+        
+    # EMAIL TOKEN HASHING
+    def set_email_token(self, email_token):
+        self.password = generate_password_hash(email_token)
+        
+    def check_email_token(self, email_token):
+      if not email_token:
+        return False
+      return check_password_hash(self.email_token, email_token)
+        
+    def set_reset_token(self, reset_token):
+      self.reset_token = generate_password_hash(reset_token)
+      
+    def check_reset_token(self, reset_token):
+      if not reset_token:
+        return False
+      return check_password_hash(self.reset_token, reset_token)
